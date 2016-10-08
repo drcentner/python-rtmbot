2693ecdbdd5a1b67a93d9910f4f7f775f8c3752a
dlehrman <dlehrman@liberty.edu>
2016-10-07 23:05:01 -0400

    ServiceNow plugin bug fixes Fixes #7 by preventing the bot from responding to its own messages using this plugin. Fixes #8 by splitting up the message looping through results.

--- docs/example-plugins/servicenow.py
+++ docs/example-plugins/servicenow.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 # don't convert to ascii in py2.7 when creating string to return
+from client import slack_client as sc
 import yaml
 import re
 
@@ -12,14 +13,31 @@ search_regex = config.get('SERVICENOW_SEARCH_REGEX')
 outputs = []
 
 
+# The RTM API sometimes processes the bot's last posted message
+# upon connection. This can result in the bot posting duplicate URL's
+# when it connects under certain circumstances (e.g. disconnect/reconnect).
+# Also, there's probably few, if any, circumstances where the bot
+# responding to messages it posted would be desired behavior.
+bot_user_id = sc.api_call('auth.test')['user_id']
+
 def process_message(data):
-    # Search the message text for patterns indicative of a ServiceNow ticket
-    ticket_search = re.search(search_regex, data['text'], re.IGNORECASE)
+    # If the message was not posted by the Slack bot
+    if data['user'] != bot_user_id:
+        # Split message content up by whitespace
+        strings = re.split('\s+', data['text'])
 
-    # If a matching pattern was found
-    if ticket_search:
-        # Format the SN search URL using the instance variable
-        ticket_url = 'https://' + instance + '.service-now.com/nav_to.do?uri=textsearch.do?sysparm_search=' + ticket_search.group()
+        # For each grouping of characters
+        for string in strings:
+            # Remove non-alphanumeric characters
+            string = re.sub('\W+', '', string)
 
-        # Output the URL
-        outputs.append([data['channel'], ticket_url])
+            # Search for a regex pattern match
+            ticket = re.match(search_regex, string, re.IGNORECASE)
+
+            # If a match was found
+            if ticket:
+                # Format the SN search URL using the instance variable
+                ticket_url = 'https://' + instance + '.service-now.com/nav_to.do?uri=textsearch.do?sysparm_search=' + ticket.group()
+
+                # Output the URL
+                outputs.append([data['channel'], ticket_url])
